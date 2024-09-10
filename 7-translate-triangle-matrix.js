876ea9c52e0816c19e7c62839f35295668543937
testcs50 <assayf95@gmail.com>
2024-09-10 05:01:59 +0300

    add matrix transformations

--- 7-translate-triangle-matrix.js
+++ 7-translate-triangle-matrix.js
@@ -1,7 +1,12 @@
 const vsSource = `
+uniform mat4 uRotateMatrix;
+uniform mat4 uTranslateMatrix;
+uniform mat4 uScaleMatrix;
+
 attribute vec4 aPosition;
+
 void main() {
-  gl_Position = aPosition;
+  gl_Position = uTranslateMatrix * uRotateMatrix * uScaleMatrix * aPosition;
 }`;
 
 const fsSource = `
@@ -57,6 +62,79 @@ if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
   gl.deleteShader(fragmentShader);
 }
 gl.useProgram(program);
+
+const uRotateMatrix = gl.getUniformLocation(program, "uRotateMatrix");
+const angle = 90;
+const radian = (angle * Math.PI) / 180;
+const cosB = Math.cos(radian);
+const sinB = Math.sin(radian);
+const rotateMatrix = new Float32Array([
+  cosB,
+  sinB,
+  0.0,
+  0.0,
+  -sinB,
+  cosB,
+  0.0,
+  0.0,
+  0.0,
+  0.0,
+  1.0,
+  0.0,
+  0.0,
+  0.0,
+  0.0,
+  1.0,
+]);
+gl.uniformMatrix4fv(uRotateMatrix, false, rotateMatrix);
+
+const uTranslateMatrix = gl.getUniformLocation(program, "uTranslateMatrix");
+const translateX = 0.5;
+const translateY = 0.5;
+const translateZ = 0.0;
+const translateMatrix = new Float32Array([
+  1.0,
+  0.0,
+  0.0,
+  0.0,
+  0.0,
+  1.0,
+  0.0,
+  0.0,
+  0.0,
+  0.0,
+  1.0,
+  0.0,
+  translateX,
+  translateY,
+  translateZ,
+  1.0,
+]);
+gl.uniformMatrix4fv(uTranslateMatrix, false, translateMatrix);
+
+const uScaleMatrix = gl.getUniformLocation(program, "uScaleMatrix");
+const scaleX = 0.5;
+const scaleY = 1.5;
+const scaleZ = 1.0;
+const scaleMatrix = new Float32Array([
+  scaleX,
+  0.0,
+  0.0,
+  0.0,
+  0.0,
+  scaleY,
+  0.0,
+  0.0,
+  0.0,
+  0.0,
+  scaleZ,
+  0.0,
+  0.0,
+  0.0,
+  0.0,
+  1.0,
+]);
+gl.uniformMatrix4fv(uScaleMatrix, false, scaleMatrix);
 
 const aPosition = gl.getAttribLocation(program, "aPosition");
 
